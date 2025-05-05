3c5a2526d22a50ff73952ed2d1cfb19ff36ff7f8
Diogo Martins Ribeiro <a47207@alunos.isel.pt>
2025-05-05 21:50:29 +0100

    updates Routing

--- KeepMyPlanet/server/src/main/kotlin/pt/isel/keepmyplanet/plugins/Routing.kt
+++ KeepMyPlanet/server/src/main/kotlin/pt/isel/keepmyplanet/plugins/Routing.kt
@@ -7,27 +7,36 @@ import io.ktor.server.routing.routing
 import pt.isel.keepmyplanet.Greeting
 import pt.isel.keepmyplanet.api.eventWebApi
 import pt.isel.keepmyplanet.api.messageWebApi
+import pt.isel.keepmyplanet.api.userWebApi
 import pt.isel.keepmyplanet.api.zoneWebApi
 import pt.isel.keepmyplanet.repository.mem.InMemoryEventRepository
 import pt.isel.keepmyplanet.repository.mem.InMemoryEventStateChangeRepository
 import pt.isel.keepmyplanet.repository.mem.InMemoryMessageRepository
+import pt.isel.keepmyplanet.repository.mem.InMemoryUserRepository
 import pt.isel.keepmyplanet.repository.mem.InMemoryZoneRepository
 import pt.isel.keepmyplanet.service.ChatSseService
 import pt.isel.keepmyplanet.service.EventService
 import pt.isel.keepmyplanet.service.EventStateChangeService
 import pt.isel.keepmyplanet.service.MessageService
+import pt.isel.keepmyplanet.service.UserService
 import pt.isel.keepmyplanet.service.ZoneService
+import pt.isel.keepmyplanet.util.PasswordHasher
 
 fun Application.configureRouting() {
     val zoneRepository = InMemoryZoneRepository()
-    val zoneService = ZoneService(zoneRepository)
+    val userRepository = InMemoryUserRepository()
     val eventRepository = InMemoryEventRepository(zoneRepository)
     val messageRepository = InMemoryMessageRepository()
-    val chatSseService = ChatSseService()
-    val messageService = MessageService(messageRepository, eventRepository, chatSseService)
-    val eventService = EventService(eventRepository, zoneRepository)
     val eventStateChangeRepository = InMemoryEventStateChangeRepository()
+
+    val zoneService = ZoneService(zoneRepository, userRepository, eventRepository)
+    val chatSseService = ChatSseService()
+    val messageService = MessageService(messageRepository, eventRepository, userRepository, chatSseService)
+    val eventService = EventService(eventRepository, zoneRepository)
     val eventStateChangeService = EventStateChangeService(eventRepository, eventStateChangeRepository)
+
+    val passwordHasher = PasswordHasher()
+    val userService = UserService(userRepository, eventRepository, passwordHasher)
 
     routing {
         get("/") {
@@ -36,6 +45,7 @@ fun Application.configureRouting() {
 
         zoneWebApi(zoneService)
         messageWebApi(messageService, chatSseService)
+        userWebApi(userService)
         eventWebApi(eventService, eventStateChangeService)
     }
 }
