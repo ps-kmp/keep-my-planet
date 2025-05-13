3b9d35e9fb7a918b810ba94108497320d7af1d2c
49423 - Rafael Pegacho <a49423@alunos.isel.pt>
2025-05-13 18:28:15 +0100

    add browser support

--- KeepMyPlanet/composeApp/src/wasmJsMain/kotlin/pt/isel/keepmyplanet/data/api/HttpClient.wasmJs.kt
+++ KeepMyPlanet/composeApp/src/wasmJsMain/kotlin/pt/isel/keepmyplanet/data/api/HttpClient.wasmJs.kt
@@ -1,5 +1,6 @@
 package pt.isel.keepmyplanet.data.api
 
 import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.js.Js
 
-actual fun httpClientEngine(): HttpClientEngineFactory<*> = TODO()
+actual fun httpClientEngine(): HttpClientEngineFactory<*> = Js
--- KeepMyPlanet/composeApp/src/wasmJsMain/kotlin/pt/isel/keepmyplanet/main.kt
+++ KeepMyPlanet/composeApp/src/wasmJsMain/kotlin/pt/isel/keepmyplanet/main.kt
@@ -1,12 +1,28 @@
+@file:Suppress("ktlint:standard:filename")
+
 package pt.isel.keepmyplanet
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import pt.isel.keepmyplanet.data.api.createHttpClient
+import pt.isel.keepmyplanet.data.service.AuthService
+import pt.isel.keepmyplanet.data.service.ChatService
+import pt.isel.keepmyplanet.data.service.UserService
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        val appViewModel =
+            remember {
+                val httpClient = createHttpClient { null }
+                AppViewModel(
+                    AuthService(httpClient),
+                    ChatService(httpClient),
+                    UserService(httpClient),
+                )
+            }
+        App(appViewModel)
     }
 }
