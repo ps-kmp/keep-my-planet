5d03736b8bea34522724c6b446d1e889ce0c3a58
rafaeldeez <101189854+rafaeldeez@users.noreply.github.com>
2025-07-27 22:21:02 +0100

    Update Config.android.kt

--- KeepMyPlanet/shared/src/androidMain/kotlin/pt/isel/keepmyplanet/Config.android.kt
+++ KeepMyPlanet/shared/src/androidMain/kotlin/pt/isel/keepmyplanet/Config.android.kt
@@ -1,4 +1,3 @@
 package pt.isel.keepmyplanet
 
-// actual val BASE_URL: String = "https://kmp-api.onrender.com"
-actual val BASE_URL: String = "http://localhost:1904"
+actual val BASE_URL: String = "https://kmp-api.onrender.com"
